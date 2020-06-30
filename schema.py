e48bce5b5103b5633f19297e2999dfd1b3853a2a
vscodr <vscodr.brad@gmail.com>
2020-06-30 18:54:36 +0800

    figured out the 'map from url' issue!

--- schema.py
+++ schema.py
@@ -1,45 +1,84 @@
-from ariadne import QueryType, gql, make_executable_schema
-from ariadne.asgi import GraphQL
+
 import json
+
 import requests
+from ariadne import ObjectType, QueryType, gql, make_executable_schema
+from ariadne.asgi import GraphQL
 
-from ariadne import ObjectType, make_executable_schema
 
-from ariadne import ObjectType, make_executable_schema
+def get_list_items(list):
+   for i in list:
+    [r] = requests.get(i)
+    print(r)
+    
+
+
+
 
 type_defs = gql("""
     type Query {
-        hello: String!
-        person(id: Int): Person
+            person(id: Int): Person
+            
+        
     },
-    type Person{
-	name: String
-	height: String
-	mass: String
-	hair_color: String
-	skin_color: String
-	eye_color: String
-	birth_year: String
-	gender: String
-    }
-""")
+        type Person{
+            name: String
+            height: String
+            mass: String
+            hair_color: String
+            skin_color: String
+            eye_color: String
+            birth_year: String
+            gender: String
+            films: [Film]
+        },
+
+        type Film {
+            title: String 
+            episode_id: Int 
+            opening_crawl: String
+            director: String 
+            producer: String 
+            release_date: String    
+            },
+
+        """)
+
+
+
+
+
+
 
 query = ObjectType("Query")
 
-@query.field("hello")
-def resolve_hello(*_):
-    return "Hello!"
 
+person = ObjectType("Person")
 @query.field("person")
 def resolve_person(self, info, id):
     response = requests.get(f'https://swapi.dev/api/people/{id}')
     res = json.loads(response.text)
-    dict_items = dict(res.items())
+    dict_items = dict(res.items()) 
+    # print("this is Person", type(dict_items),  json.dumps(dict_items, indent = 4))   
     return dict_items
 
+@person.field("films")
+def resolve_films(Person, info):
+    _f = Person['films']
+    films = []
+    for i in _f:
+       r = requests.get(i)
+       r_dict = r.json()
+       films.append(r_dict)
+    return films
+    
+ 
+   
+    
+  
 
-schema = make_executable_schema(type_defs, query)
 
 
-schema = make_executable_schema(type_defs, query)
+schema = make_executable_schema(type_defs, query, person)
+
 app = GraphQL(schema, debug=True)
