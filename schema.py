2ef2876c6787e14377cd013fef8d9f7ad2af2d49
vscodr <vscodr.brad@gmail.com>
2020-07-01 08:11:44 +0800

    now with asyn and await

--- schema.py
+++ schema.py
@@ -1,25 +1,15 @@
-
+from asyncio import gather
 import json
-
 import requests
 from ariadne import ObjectType, QueryType, gql, make_executable_schema
 from ariadne.asgi import GraphQL
-
-
-def get_list_items(list):
-   for i in list:
-    [r] = requests.get(i)
-    print(r)
-    
-
-
-
+import httpx 
+from httpx import AsyncClient
 
 type_defs = gql("""
     type Query {
             person(id: Int): Person
-            
-        
+            filme : Film        
     },
         type Person{
             name: String
@@ -44,40 +34,28 @@ type_defs = gql("""
 
         """)
 
-
-
-
-
-
-
 query = ObjectType("Query")
-
 
 person = ObjectType("Person")
 @query.field("person")
-def resolve_person(self, info, id):
-    response = requests.get(f'https://swapi.dev/api/people/{id}')
-    res = json.loads(response.text)
-    dict_items = dict(res.items()) 
-    # print("this is Person", type(dict_items),  json.dumps(dict_items, indent = 4))   
-    return dict_items
+async def resolve_person(self, info, id):
+    async with httpx.AsyncClient() as client:
+        r = await client.get(f'https://swapi.dev/api/people/{id}')
+        #r.json()  
+    return r.json()
 
 @person.field("films")
-def resolve_films(Person, info):
-    _f = Person['films']
+
+async def resolve_films(person, _):
     films = []
-    for i in _f:
-       r = requests.get(i)
-       r_dict = r.json()
-       films.append(r_dict)
-    return films
-    
- 
-   
-    
-  
-
-
+    async with httpx.AsyncClient() as client:
+        for i in person['films']:
+            r = await client.get(i) 
+            films.append(r.json())
+        return films    
+        
+     
+film = ObjectType("Film")
 
 schema = make_executable_schema(type_defs, query, person)
 
